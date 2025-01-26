ab84db72b3089d58185d6473c2fa25b2757fd515
sohaghamjah <aymansohag.contact@gmail.com>
2025-01-26 14:24:23 +0600

    OrderHeader Model

--- FastFood.Models/obj/Debug/net8.0/FastFood.Models.AssemblyInfo.cs
+++ FastFood.Models/obj/Debug/net8.0/FastFood.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastFood.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298efda32b49730a4074fc292c8dd907448565f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastFood.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastFood.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
